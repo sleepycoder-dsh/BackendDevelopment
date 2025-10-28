920dea74760ffda653ff52c33a828c0331a4f442
Sri Harshini <dsriharshini2000@gmail.com>
2025-10-27 20:26:18 -0500

    Updated code comments for more readability

--- src/main/java/com/harshini/bookreview/repository/BookReviewRepository.java
+++ src/main/java/com/harshini/bookreview/repository/BookReviewRepository.java
@@ -1,11 +1,14 @@
 package com.harshini.bookreview.repository;
 
+// Imports JPA repository features
 import com.harshini.bookreview.model.BookReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+// Marks this interface as a Spring repository
 import org.springframework.stereotype.Repository;
 
+// JpaRepository provides all  CRUD operations automatically
 @Repository
 public interface BookReviewRepository extends JpaRepository<BookReview, Long> {
-    // JpaRepository provides all basic CRUD operations automatically
-}
 
+}
